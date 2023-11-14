b8135ebd383d71ccb578fbd8354d4f9a8fc59bb9
elisabeth <elisabeth-kluth@t-online.de>
2023-11-14 05:27:37 +0100

    add moving box

--- main.cpp
+++ main.cpp
@@ -16,6 +16,12 @@ double torus(glm::dvec3 p) {
     return sqrt(x * x + p.y * p.y) - r2;
 }
 
+double box( glm::dvec3 p, glm::dvec3 b )
+{
+    glm::dvec3 q = abs(p) - b;
+    return glm::length(max(q,0.0)) + std::min(std::max(q.x,std::max(q.y,q.z)),0.0);
+}
+
 double combine(double d1, double d2) {
     return std::min(d1, d2);
 }
@@ -26,7 +32,8 @@ struct Edge {
     int idx;
 };
 
-std::vector<glm::dvec3> mesh_generator(std::function<double(glm::dvec3)> f, int n = 50) {
+std::pair<std::vector<glm::dvec3>, std::vector<std::array<int, 4>>>
+mesh_generator(std::function<double(glm::dvec3)> f, int n = 50) {
     glm::dvec3 lower{-3};
     glm::dvec3 upper{3};
 
@@ -44,6 +51,7 @@ std::vector<glm::dvec3> mesh_generator(std::function<double(glm::dvec3)> f, int 
         }
     }
     std::vector<glm::dvec3> points;
+    std::vector<std::array<int, 4>> faces;
     std::vector<Edge> edges = {
             {{1, 1, 0}, {1, 1, 1}, 2},
             {{1, 0, 1}, {1, 1, 1}, 1},
@@ -63,7 +71,8 @@ std::vector<glm::dvec3> mesh_generator(std::function<double(glm::dvec3)> f, int 
             {{1, 0, 1}, {1, 1, 1}},
             {{0, 1, 1}, {1, 1, 1}},
     };
-    std::vector<glm::dvec3> center_points(n * n * n, {0, 0, 0});
+
+    std::vector<int> index_points(n * n * n, -1);
     for (int i = 0; i < n - 1; i++) {
         for (int j = 0; j < n - 1; j++) {
             for (int k = 0; k < n - 1; k++) {
@@ -82,7 +91,8 @@ std::vector<glm::dvec3> mesh_generator(std::function<double(glm::dvec3)> f, int 
                     }
                 }
                 if (counter != 0) {
-                    center_points[i * n * n + j * n + k] = center_p / (double) counter;
+                    points.push_back(center_p / (double) counter);
+                    index_points[i * n * n + j * n + k] = (int) points.size() - 1;
                 }
             }
         }
@@ -91,53 +101,54 @@ std::vector<glm::dvec3> mesh_generator(std::function<double(glm::dvec3)> f, int 
     for (int i = 0; i < n - 1; i++) {
         for (int j = 0; j < n - 1; j++) {
             for (int k = 0; k < n - 1; k++) {
-                glm::dvec3 p = center_points[i * n * n + j * n + k];
+                int index_p = index_points[i * n * n + j * n + k];
                 for (auto e: edges) {
                     glm::ivec3 index_p1 = {i + e.a.x, j + e.a.y, k + e.a.z};
                     glm::ivec3 index_p2 = {i + e.b.x, j + e.b.y, k + e.b.z};
                     double v1 = grid[index_p1.x * n * n + index_p1.y * n + index_p1.z];
                     double v2 = grid[index_p2.x * n * n + index_p2.y * n + index_p2.z];
                     if (v1 * v2 <= 0) {
+                        std::array<int, 4> face{};
                         if (e.idx == 0) {
-                            points.push_back(p);
-                            points.push_back(center_points[i * n * n + j * n + k + 1]);
-                            points.push_back(center_points[i * n * n + (j + 1) * n + k + 1]);
-                            points.push_back(center_points[i * n * n + (j + 1) * n + k]);
+                            face[0] = index_p;
+                            face[1] = index_points[i * n * n + j * n + k + 1];
+                            face[2] = index_points[i * n * n + (j + 1) * n + k + 1];
+                            face[3] = index_points[i * n * n + (j + 1) * n + k];
                         }
                         if (e.idx == 1) {
-                            points.push_back(p);
-                            points.push_back(center_points[(i + 1) * n * n + j * n + k]);
-                            points.push_back(center_points[(i + 1) * n * n + j * n + k + 1]);
-                            points.push_back(center_points[i * n * n + j * n + k + 1]);
+                            face[0] = index_p;
+                            face[1] = index_points[(i + 1) * n * n + j * n + k];
+                            face[2] = index_points[(i + 1) * n * n + j * n + k + 1];
+                            face[3] = index_points[i * n * n + j * n + k + 1];
                         }
                         if (e.idx == 2) {
-                            points.push_back(p);
-                            points.push_back(center_points[i * n * n + (j + 1) * n + k]);
-                            points.push_back(center_points[(i + 1) * n * n + (j + 1) * n + k]);
-                            points.push_back(center_points[(i + 1) * n * n + j * n + k]);
+                            face[0] = index_p;
+                            face[1] = index_points[i * n * n + (j + 1) * n + k];
+                            face[2] = index_points[(i + 1) * n * n + (j + 1) * n + k];
+                            face[3] = index_points[(i + 1) * n * n + j * n + k];
                         }
                         if (v1 < 0) {
-                            std::reverse(points.end()-4, points.end());
+                            std::reverse(face.begin(), face.end());
                         }
+                        faces.push_back(face);
                     }
                 }
             }
         }
     }
-    return points;
+    return {points, faces};
 }
 
 void callback() {
     double t = ImGui::GetTime();
-    std::vector<glm::dvec3> mesh_points = mesh_generator([=](glm::dvec3 v) {
-        return combine(sphere(v, 0.25, 0.85 * glm::dvec3{std::cos(t), 0, std::sin(t)}), torus(v));
-    }, 50);
-    std::vector<std::array<int, 4>> faces;
-    for (int i = 0; i < mesh_points.size() - 3; i++) {
-        std::array<int, 4> a{i, ++i, ++i, ++i};
-        faces.push_back(a);
-    }
-    ps::registerSurfaceMesh("my mesh", mesh_points, faces);
+    glm::dvec3 moving_center = 0.85 * glm::dvec3{std::cos(t), 0, std::sin(t)};
+    /*auto mesh_circle = mesh_generator([=](glm::dvec3 v) {
+        return combine(sphere(v, 0.25, moving_center), torus(v));
+    }, 100);*/
+    auto mesh_box = mesh_generator([=](glm::dvec3 v) {
+        return combine(box(v-moving_center, {0.2,0.2,0.2}), torus(v));
+    }, 100);
+    ps::registerSurfaceMesh("my mesh box", mesh_box.first, mesh_box.second);
 }
 
 
